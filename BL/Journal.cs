9b55c4cc6f86217d308bf5e585fa58cec6c35c87
906 <906@SHACHAM3>
2017-02-01 10:55:07 +0200

    Add project files.

---
+++ BL/Journal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BookLib
+{
+    public class Journal : AbstractItem
+    {
+        public Journal(string name, DateTime printDate) : base(name, printDate)
+        {
+
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookLib;
+
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Book b = new Book("Boker Tov", DateTime.Now);
+        }
+    }
+}
